e8470e723ca0e3f7bc9bf62a8264997c6db27458
20171105409ani <2509296163@qq.com>
2018-10-12 20:42:05 +0800

    排序
    倒序

--- sort/main.swift
+++ sort/main.swift
@@ -7,6 +7,19 @@
 //
 
 import Foundation
+var array:[Int]=[0,1,2,3,4,5,6,7,8,9]
+for i in 0..<array.count{
+    for j in 0..<array.count-(i+1){
+        if array[j]>array[j+1]{
+            let temp=array[j]
+            array[j]=array[j+1]
+            array[j+1]=temp
+        }
+    }
+}
+for i in 0..<array.count{
+    print(array[i])
+}
 
-print("Hello, World!")
+
 
